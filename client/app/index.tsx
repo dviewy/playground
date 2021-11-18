6cda9988bacd7321a2c40edc4279851bb148e8a1
dviewy <dviewy@gmail.com>
2021-11-18 15:49:14 +0300

    Removed react-router-config

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import { matchRoutes, renderRoutes } from "react-router-config";
 import { GlobalStyle } from "./global-style";
 
 export const TestPage1 = () => <h1>test page 1</h1>;
@@ -10,24 +9,5 @@ export const TestPage3 = () => <h1>test page 3</h1>;
 export const App = () => (
   <>
     <GlobalStyle />
-    <BrowserRouter>
-      {renderRoutes([
-        {
-          path: "/",
-          exact: true,
-          component: TestPage1,
-        },
-        {
-          path: "/test-2",
-          exact: true,
-          component: TestPage2,
-        },
-        {
-          path: "/test-3",
-          exact: true,
-          component: TestPage3,
-        },
-      ])}
-    </BrowserRouter>
   </>
 );
